407a4e7644a72908c973f1ff98ccd50ba6312e71
Mohammed Miessal <mohammedmiessal@gmail.com>
2024-11-03 18:05:44 +0100

    Créer pour Login User Story

---
+++ src/main/java/com/maska/h_l/service/AuthService.java
@@ -0,0 +1,12 @@
+package com.maska.h_l.service;
+
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthService {
+
+
+
+
+}
---
+++ src/main/java/com/maska/h_l/web/rest/AuthController.java
@@ -0,0 +1,13 @@
+    package com.maska.h_l.web.rest;
+
+
+    import org.springframework.web.bind.annotation.*;
+
+
+    @RestController
+    @RequestMapping("/api/auth")
+    public class AuthController {
+
+
+    }
+
